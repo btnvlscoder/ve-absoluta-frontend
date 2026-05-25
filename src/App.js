dfca0f4396d4e004d1cbf1517c142109e5ecce9e
btnvlscoder <btnvlscoder@gmail.com>
2026-05-25 02:52:36 -0400

    Integracion de interfaz historial de casos. MainLayout

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import MainLayout from './MainLayout'; // Tu nuevo cascarón con el Sidebar
-
-// Vistas
-import DashboardGlobal from './DashboardGlobal';
-import AnalisisView from './AnalisisView'; // El que acabas de renombrar
-import HistorialCasos from './HistorialCasos';
+import MainLayout from './components/MainLayout'; 
+import DashboardGlobal from './components/DashboardGlobal';
+import AnalisisView from './components/AnalisisView'; 
+import HistorialCasos from './components/HistorialCasos';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* MainLayout envuelve todas las rutas para que el menú siempre esté visible */}
         <Route path="/" element={<MainLayout />}>
-          
-          {/* Rutas internas que se inyectan en el <Outlet /> del MainLayout */}
           <Route index element={<DashboardGlobal />} />
           <Route path="analisis" element={<AnalisisView />} />
           <Route path="historial" element={<HistorialCasos />} />
-          
-          {/* Ruta por defecto por si alguien escribe una URL que no existe */}
           <Route path="*" element={<Navigate to="/" replace />} />
-          
         </Route>
       </Routes>
     </BrowserRouter>
